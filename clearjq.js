7dac6f9e68c85822bf9dc1f4d393c5639d174ab0
Examblog64 <64898760+krooluang@users.noreply.github.com>
2023-11-29 14:52:10 +0700

    Add files via upload

---
+++ clearjq.js
@@ -0,0 +1,7 @@
+  if (window.trustedTypes && window.trustedTypes.createPolicy) {
+    window.trustedTypes.createPolicy('default', {
+      createHTML: string => string,
+      createScriptURL: string => string,
+      createScript: string => string,
+    });
+  }
